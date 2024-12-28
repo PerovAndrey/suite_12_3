47a1f37b460cd4b18a77857cd3e470a96739a232
PerovAndrey <nikita.vasiliev1982@mail.ru>
2024-12-28 12:49:30 +0300

    Add files via upload

---
+++ suite_12_3.py
@@ -0,0 +1,133 @@
+import unittest
+import rt
+
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_walk(self):
+        runner = Runner("Petr")
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_run(self):
+        runner = Runner("Bob")
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_challenge(self):
+        runner1 = Runner("Fedor")
+        runner2 = Runner("Anna")
+        for i in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+
+class TournamentTest(unittest.TestCase):
+    is_frozen = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def setUp(self):
+        self.runer_1 = Runner('Усейн', 10)
+        self.runer_2 = Runner('Андрей', 9)
+        self.runer_3 = Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for test_key, test_value in cls.all_results.items():
+            print(f'Тест: {test_key}')
+            for key, value in test_value.items():
+                print(f'\t{key}: {value.name}')
+
+    def test_turn1(self):
+        turn_1 = Tournament(90, self.runer_1, self.runer_3)
+        result = turn_1.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn1'] = result
+
+    def test_turn2(self):
+        turn_2 = Tournament(90, self.runer_2, self.runer_3)
+        result = turn_2.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn2'] = result
+
+    def test_turn3(self):
+        turn_3 = Tournament(90, self.runer_1, self.runer_2, self.runer_3)
+        result = turn_3.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn3'] = result
+
+    def test_turn4(self):
+
+        turn_4 = Tournament(6, self.runer_1, self.runer_2, self.runer_3)
+        result = turn_4.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn4'] = result
+
+if __name__ == '__main__':
+    unittest.main()
+
+run_tourST = unittest.TestSuite()
+run_tourST.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))
+run_tourST.addTest(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))
+
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(run_tourST)
